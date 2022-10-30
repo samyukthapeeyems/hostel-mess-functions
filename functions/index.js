88bd5172db06a265279f66629ea076759528511c
samyukthapeeyems <samyukthapeeyems@gmail.com>
2022-10-30 20:50:22 +0530

    slight changes

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
 import * as functions from "firebase-functions";
-import { initializeApp, cert} from "firebase-admin/app";
+import { initializeApp, cert , getApps } from "firebase-admin/app";
 import serviceAccount from "../service_account.json" assert { type: 'json'};
-import {createOrder} from "./order.js";
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -10,17 +9,49 @@ import {createOrder} from "./order.js";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+import { getFirestore, Timestamp, FieldValue, FieldPath } from 'firebase-admin/firestore';
 
-initializeApp({
+const app = initializeApp({
     credential: cert(serviceAccount)
 });
+const db = getFirestore();
+
+
+
+
+
+import { createOrder } from "./order.js";
+
 
 export const createOrderx = functions.https.onRequest(async (req, res) => {
-    try{
-        let result = await createOrder(req.body.itemList, null);
+    try {
+
+        //   functions.logger.log(db)
+        
+
+        let result = [];
+
+
+        const snapShot = await db.collection('items').doc('mHshUOjCEgETWxAx2eFX').get()
+        // let snapShot = await itemC.get()
+        // where("name", "==", "meal").get();
+
+
+        if (snapShot.empty) {
+            functions.logger.log('No matching documents.');
+        }
+        functions.logger.log(snapShot.data())
+        snapShot.forEach(doc => {
+            functions.logger.log(doc);
+            result.push(doc.data());
+        })
+
+        functions.logger.log(snapShot.data());
+
+        //let result = await createOrder(req.body.itemList, null);
         res.json(result);
     }
-    catch(e){
+    catch (e) {
         res.json(e);
     }
 })
--- functions/order.js
+++ functions/order.js
@@ -2,24 +2,49 @@ import { getFirestore, Timestamp, FieldValue, FieldPath } from 'firebase-admin/f
 import functions from "firebase-functions";
 
 
-export async function createOrder(itemList, userId){
+export async function createOrder(itemList, userId) {
     const db = getFirestore();
+        //   functions.logger.log(db)
+
     let result = [];
-    try{
-        const snapShot = await db.collection("items").where("name", "==", "meal").get();
-        snapShot.forEach(doc => {
-            result.push(doc);
-        })
-        functions.logger.log(result);
+
+    try {
+
+        const docRef = db.collection('items').doc('alovelace');
+
+        await docRef.set({
+            first: 'Ada',
+            last: 'Lovelace',
+            born: 1815
+        });
+
+        const snapShot = await db.collection('items').doc('mHshUOjCEgETWxAx2eFX').get()
+        // let snapShot = await itemC.get()
+        // where("name", "==", "meal").get();
+
+
+        if (snapShot.empty) {
+            functions.logger.log('No matching documents.');
+        }
+
+
+
+        // snapShot.forEach(doc => {
+
+        //     functions.logger.log(doc);
+
+        //     result.push(doc.data());
+        // })
+        functions.logger.log(snapShot.data());
 
         return result;
     }
-    catch(e){
+    catch (e) {
         throw e;
     }
 }
 
-export function cancelOrder(){}
+export function cancelOrder() { }
 
-export function updateStatus(){}
+export function updateStatus() { }
 
