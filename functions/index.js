58a7b3a04209c8bc4a2a4647f6208e9093824cc7
samyukthapeeyems <samyukthapeeyems@gmail.com>
2022-11-27 23:10:34 +0530

    added function for setting metadata for image upload event

--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,14 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { createOrder } from "./src/order.js";
 import { getAuth } from "firebase-admin/auth";
 import { addItemImage } from "./src/item.js";
+import { setMetadata } from "./src/storage.js";
 // import serviceAccount from "../service_account.json";
 
 
 
 import { createRequire } from "module"; // Bring in the ability to create the 'require' method
 const require = createRequire(import.meta.url); // construct the require method
-const serviceAccount = require("./service_account.json") // use the require method
+const serviceAccount = require("../service_account.json") // use the require method
 
 
 initializeApp({
@@ -37,15 +38,21 @@ export const processUserSignUp = regionalFunction.auth.user().onCreate(async (us
  
 })
 
-async function setRole(uid,roles){
-    await getAuth().setCustomUserClaims(uid, {
-        roles: roles
-    })
-}
+// async function setRole(uid,roles){
+//     await getAuth().setCustomUserClaims(uid, {
+//         roles: roles
+//     })
+// }
 
 //setRole("9bKbE6JvgdeZiPbLG324l83U2oF3",["admin"])
 
-export const addImage = functions.storage.object().onFinalize(obj => addItemImage(obj));
+export const addImage = functions.storage.object().onFinalize(async obj => {
+    let newMetadata = {
+        "Cache-Control" : "max-age=2592000"
+    }
+    await setMetadata(newMetadata, obj.name);
+    return addItemImage(obj)
+});
 // export const createOrderx = functions.https.onCall(async (data, context) => {
 //     try {
 //         let result = await createOrder(data.order, context.);
--- functions/src/storage.js
+++ functions/src/storage.js
@@ -1,6 +1,17 @@
+import { getStorage } from 'firebase-admin/storage';
 
-
-function storeImage(obj){
-    let {name, metadata} = obj;
-    
+export async function setMetadata(metadataObj, path){
+    if(typeof metadataObj !== "object" && typeof path !== "string"){
+        throw new Error("invalid params");
+    }
+    const storage = getStorage();
+    let pathArr = path.split(/\//); 
+    try{
+        let result = await storage.bucket(pathArr[0]).file(pathArr[1]).setMetadata(metadataObj);
+        return result;
+    }
+    catch(e){
+        console.log(e);
+        throw e
+    }
 }
